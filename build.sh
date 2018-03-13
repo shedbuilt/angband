a4cb583a49f49d293545457ac1899ffe467869fe
Auston Stewart <auston@shedbuilt.net>
2018-03-12 18:42:34 -1000

    Updating to use sdl

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-./autogen.sh
-./configure --prefix=/usr
-make -j $SHED_NUMJOBS
-make "DESTDIR=$SHED_FAKEROOT" install
-install -Dm755 "${SHED_FAKEROOT}/usr/games/angband" "${SHED_FAKEROOT}/usr/bin/angband"
-rmdir "${SHED_FAKEROOT}/usr/games"
+./autogen.sh &&
+./configure --prefix=/usr \
+            --enable-sdl &&
+make -j $SHED_NUMJOBS &&
+make DESTDIR="$SHED_FAKEROOT" install &&
+install -v -Dm755 "${SHED_FAKEROOT}/usr/games/angband" "${SHED_FAKEROOT}/usr/bin/angband" &&
+rm -rfv "${SHED_FAKEROOT}/usr/games"
